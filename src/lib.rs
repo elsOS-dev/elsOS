37a1d109ede688c50b9cc2a6085476260b4972cc
afrangio <afrangio@student.42.fr>
2022-01-16 18:02:43 +0100

    Implémente le début du VGA text buffer

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,22 @@
+#![feature(exclusive_range_pattern)]
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
 extern crate rlibc;
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
-pub extern "C" fn kernel_main() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop {}
+pub extern "C" fn kernel_main() -> !
+{
+	vga_buffer::write("Hello, elsos world !");
+	loop
+	{}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+fn panic(_info: &PanicInfo) -> !
+{
+	loop
+	{}
 }
-
-
-
-
-
-
-
