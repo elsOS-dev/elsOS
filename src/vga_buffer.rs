b27a456ccd0e7cbb3fdb76996bf926d962c8a235
afrangio <afrangio@student.42.fr>
2022-01-16 22:51:49 +0100

    Rend fonctionnel le \n ainsi que la macro println

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -66,6 +66,7 @@ impl Writer
 		if byte == b'\n' || self.column_position >= BUFFER_WIDTH
 		{
 			self.new_line();
+			return ;
 		}
 
 		let row = BUFFER_HEIGHT - 1;
@@ -89,16 +90,32 @@ impl Writer
 			}
 		}
 	}
+	fn clear_row(&mut self, row: usize)
+	{
+		let blank = ScreenChar
+		{
+            character: b' ',
+            color_code: self.color_code,
+        };
+
+		for col in 0..BUFFER_WIDTH
+		{
+			self.buffer.chars[row][col] = blank;
+		}
+	}
 
 	fn new_line(&mut self)
 	{
-		//		for row in 1..BUFFER_HEIGHT
-		//		{
-		//			for col in 0..BUFFER_WIDTH
-		//			{
-		//
-		//			}
-		//		}
+		for row in 1..BUFFER_HEIGHT
+		{
+			for col in 0..BUFFER_WIDTH
+			{
+				let character = self.buffer.chars[row][col];
+				self.buffer.chars[row - 1][col] = character;
+			}
+		}
+		self.clear_row(BUFFER_HEIGHT - 1);
+		self.column_position = 0;
 	}
 }
 
@@ -111,15 +128,33 @@ impl fmt::Write for Writer
 	}
 }
 
-pub fn write(s: &str)
-{
-	use core::fmt::Write;
-	let mut w = Writer {
-		column_position: 0,
-		color_code: ColorCode::new(Color::Green, Color::Black),
-		buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-	};
-	w.write_string(s);
+use lazy_static::lazy_static;
+use spin::Mutex;
 
-	write!(w, "The numbers are {} and {}", 42, 2.0 / 3.0).unwrap();
+lazy_static!
+{
+	pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer
+	{
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
 }
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
+
