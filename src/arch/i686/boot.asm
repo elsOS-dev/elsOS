9ac0ee62b5663b3111d23ac112c57fe2fe221095
Edgar Fouillet <git@edgar.fouillet.eu>
2022-01-24 16:17:32 +0100

    boot: fix asm code, add stack and esp init

--- src/arch/i686/boot.asm
+++ src/arch/i686/boot.asm
@@ -1,11 +1,24 @@
 global start
 
+section .bss
+align 16
+stack_bottom:
+resb 16384 ; 16 KiB
+stack_top:
+
 section .text
+global _start:function (_start.end - _start)
 bits 32
-start:
+_start:
+	mov esp, stack_top
+
 	extern kernel_main
 	call kernel_main
+
 	; print `OK` to screen
 	mov dword [0xb8000], 0x2f4b2f4f
-	hlt
 
+.hang:
+	hlt
+	jmp .hang
+.end:
