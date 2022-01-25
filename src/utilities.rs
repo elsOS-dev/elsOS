3d9c33f31b28aabff67b11293884e801cfeb0024
afrangio <afrangio@student.42.fr>
2022-01-25 17:04:02 +0100

    utils: add outw and move inb/outb to utils file

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,3 +1,37 @@
+use core::arch::asm;
+
+#[inline(always)]
+pub fn outw(port: u32, value: u16)
+{
+    unsafe
+    {
+        asm!("out dx, ax", in("dx") port, in("ax") value,
+                options(nomem, nostack, preserves_flags));
+    }
+}
+
+#[inline(always)]
+pub fn inb(port: u32) -> u8
+{
+    let ret: u8;
+    unsafe
+    {
+        asm!("in al, dx", out("al") ret, in("dx") port,
+                options(nomem, nostack, preserves_flags));
+    }
+    ret
+}
+
+#[inline(always)]
+pub fn outb(port: u32, value: u8)
+{
+    unsafe
+    {
+        asm!("out dx, al", in("dx") port, in("al") value,
+                options(nomem, nostack, preserves_flags));
+    }
+}
+
 pub fn get_bit_at(input: u8, n: u8) -> bool
 {
 	if n < 8
