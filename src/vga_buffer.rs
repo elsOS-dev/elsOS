f9359b6df3292adc881535a6205d16b65f4a4ed6
afrangio <afrangio@student.42.fr>
2022-01-25 19:23:01 +0100

    vga: manage backspace key

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,5 @@
 use core::fmt;
+use crate::println;
 
 const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
@@ -63,20 +64,22 @@ impl Writer
 {
 	pub fn write_byte(&mut self, byte: u8)
 	{
-		if byte == b'\n' || self.column_position >= BUFFER_WIDTH
+		match byte
 		{
-			self.new_line();
-			return ;
+			b'\n' => self.new_line(),
+			0x08  => self.backspace(),
+			_ =>
+			{
+				let row = BUFFER_HEIGHT - 1;
+				let col = self.column_position;
+
+				Writer::buffer().chars[row][col] = ScreenChar {
+					character: byte,
+					color_code: self.color_code,
+				};
+				self.column_position += 1;
+			},
 		}
-
-		let row = BUFFER_HEIGHT - 1;
-		let col = self.column_position;
-
-		Writer::buffer().chars[row][col] = ScreenChar {
-			character: byte,
-			color_code: self.color_code,
-		};
-		self.column_position += 1;
 	}
 
 	pub fn write_string(&mut self, s: &str)
@@ -85,7 +88,7 @@ impl Writer
 		{
 			match byte
 			{
-				0x20..0x7e | b'\n' => self.write_byte(byte),
+				0x20..0x7e | b'\n' | 0x08 => self.write_byte(byte),
 				_ => self.write_byte(0xfe),
 			}
 		}
@@ -116,6 +119,20 @@ impl Writer
 		}
 		self.clear_row(BUFFER_HEIGHT - 1);
 		self.column_position = 0;
+	}
+	fn backspace(&mut self)
+	{
+		if self.column_position > 0
+		{
+			let blank = ScreenChar
+			{
+				character: b' ',
+				color_code: self.color_code,
+			};
+
+			self.column_position -= 1;
+			Writer::buffer().chars[BUFFER_HEIGHT - 1][self.column_position] = blank;
+		}
 	}
 }
 
