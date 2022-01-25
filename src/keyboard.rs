b9393ec491abeacc7fe2deed518eb9d43748a16a
afrangio <afrangio@student.42.fr>
2022-01-25 20:22:40 +0100

    utils: remove outb/inb functions from keyboard

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,28 +1,5 @@
-use core::arch::asm;
 use crate::println;
 use crate::print;
-
-#[inline(always)]
-pub fn inb(port: u32) -> u8
-{
-    let ret: u8;
-    unsafe
-    {
-        asm!("in al, dx", out("al") ret, in("dx") port,
-                options(nomem, nostack, preserves_flags));
-    }
-    ret
-}
-
-
-pub fn outb(port: u32, value: u8)
-{
-    unsafe
-    {
-        asm!("out dx, al", in("dx") port, in("al") value,
-                options(nomem, nostack, preserves_flags));
-    }
-}
 
 const KEYBOARD_DATA: u32 = 0x60;
 const KEYBOARD_READ_STATUS: u32 = 0x64;
@@ -166,7 +143,7 @@ pub fn get_scancodes()
 	let mut scancode: u8 = 0;
 	loop
     {
-		let new_scancode = inb(KEYBOARD_DATA);
+		let new_scancode = crate::utilities::inb(KEYBOARD_DATA);
 
 		if new_scancode == scancode
 		{
