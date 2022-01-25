209c07da9a14c6a10020ebad1f96ea4eca2ff41b
Edgar Fouillet <git@edgar.fouillet.eu>
2022-01-25 01:53:20 +0100

    keyboard: implement shift / ctrl

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -28,6 +28,137 @@ const KEYBOARD_DATA: u32 = 0x60;
 const KEYBOARD_READ_STATUS: u32 = 0x64;
 const KEYBOARD_WRITE_COMMAND: u32 = 0x64;
 
+struct KeyboardStates
+{
+    is_shift: bool,
+    is_ctrl: bool,
+}
+
+static mut keyboard_states: KeyboardStates = KeyboardStates
+{
+    is_shift: false,
+    is_ctrl: false,
+};
+
+fn char_from_scancode(scancode: u8) -> Option<char>
+{
+	unsafe
+	{
+		if keyboard_states.is_shift
+		{
+			return match scancode
+			{
+				0x02 => Some('1'),
+				0x03 => Some('2'),
+				0x04 => Some('3'),
+				0x05 => Some('4'),
+				0x06 => Some('5'),
+				0x07 => Some('6'),
+				0x08 => Some('7'),
+				0x09 => Some('8'),
+				0x0a => Some('9'),
+				0x0b => Some('0'),
+				0x0c => Some('°'),
+				0x0d => Some('_'),
+				0x10 => Some('A'),
+				0x11 => Some('Z'),
+				0x12 => Some('E'),
+				0x13 => Some('R'),
+				0x14 => Some('T'),
+				0x15 => Some('Y'),
+				0x16 => Some('U'),
+				0x17 => Some('I'),
+				0x18 => Some('O'),
+				0x19 => Some('P'),
+				0x1A => Some('¨'),
+				0x1B => Some('*'),
+				0x1C => Some('\n'),
+				0x1E => Some('Q'),
+				0x1F => Some('S'),
+				0x20 => Some('D'),
+				0x21 => Some('F'),
+				0x22 => Some('G'),
+				0x23 => Some('H'),
+				0x24 => Some('J'),
+				0x25 => Some('K'),
+				0x26 => Some('L'),
+				0x27 => Some('M'),
+				0x28 => Some('%'),
+				0x29 => Some('>'),
+				0x2B => Some('£'),
+				0x2C => Some('W'),
+				0x2D => Some('X'),
+				0x2E => Some('C'),
+				0x2F => Some('V'),
+				0x30 => Some('B'),
+				0x31 => Some('N'),
+				0x32 => Some('?'),
+				0x33 => Some('.'),
+				0x34 => Some('/'),
+				0x35 => Some('+'),
+				0x39 => Some(' '),
+				_ => None,
+			};
+		}
+		else
+		{
+			return match scancode
+			{
+				0x02 => Some('&'),
+				0x03 => Some('é'),
+				0x04 => Some('"'),
+				0x05 => Some('\''),
+				0x06 => Some('('),
+				0x07 => Some('§'),
+				0x08 => Some('è'),
+				0x09 => Some('!'),
+				0x0a => Some('ç'),
+				0x0b => Some('à'),
+				0x0c => Some(')'),
+				0x0d => Some('-'),
+				0x10 => Some('a'),
+				0x11 => Some('z'),
+				0x12 => Some('e'),
+				0x13 => Some('r'),
+				0x14 => Some('t'),
+				0x15 => Some('y'),
+				0x16 => Some('u'),
+				0x17 => Some('i'),
+				0x18 => Some('o'),
+				0x19 => Some('p'),
+				0x1A => Some('^'),
+				0x1B => Some('$'),
+				0x1C => Some('\n'),
+				0x1E => Some('q'),
+				0x1F => Some('s'),
+				0x20 => Some('d'),
+				0x21 => Some('f'),
+				0x22 => Some('g'),
+				0x23 => Some('h'),
+				0x24 => Some('j'),
+				0x25 => Some('k'),
+				0x26 => Some('l'),
+				0x27 => Some('m'),
+				0x28 => Some('ù'),
+				0x29 => Some('<'),
+				0x2B => Some('`'),
+				0x2C => Some('w'),
+				0x2D => Some('x'),
+				0x2E => Some('c'),
+				0x2F => Some('v'),
+				0x30 => Some('b'),
+				0x31 => Some('n'),
+				0x32 => Some(','),
+				0x33 => Some(';'),
+				0x34 => Some(':'),
+				0x35 => Some('='),
+				0x39 => Some(' '),
+				_ => None,
+			};
+		}
+	}
+}
+
 pub fn get_scancodes()
 {
 	let mut scancode: u8 = 0;
@@ -40,76 +171,29 @@ pub fn get_scancodes()
 			continue;
 		}
 		scancode = new_scancode;
-		let key = match scancode
-		{
-			0x02 => Some('1'),
-			0x03 => Some('2'),
-			0x04 => Some('3'),
-			0x05 => Some('4'),
-			0x06 => Some('5'),
-			0x07 => Some('6'),
-			0x08 => Some('7'),
-			0x09 => Some('8'),
-			0x0a => Some('9'),
-			0x0b => Some('0'),
-			0x0c => Some(')'),
-			0x0d => Some('-'),
-			// 0x0e => Some(''), Backspace
-			// 0x0f => Some(''),
-			0x10 => Some('a'),
-			0x11 => Some('z'),
-			0x12 => Some('e'),
-			0x13 => Some('r'),
-			0x14 => Some('t'),
-			0x15 => Some('y'),
-			0x16 => Some('u'),
-			0x17 => Some('i'),
-			0x18 => Some('o'),
-			0x19 => Some('p'),
-			0x1A => Some('^'),
-			0x1B => Some('$'),
-			0x1C => Some('\n'),
-			//0x1D => Some(''), ctrl gauche
-			0x1E => Some('q'),
-			0x1F => Some('s'),
-			0x20 => Some('d'),
-			0x21 => Some('f'),
-			0x22 => Some('g'),
-			0x23 => Some('h'),
-			0x24 => Some('j'),
-			0x25 => Some('k'),
-			0x26 => Some('l'),
-			0x27 => Some('m'),
-			0x28 => Some('ù'),
-			0x29 => Some('`'),
-			//0x2A => Some(''),
-			//0x2B => Some(''),
-			0x2C => Some('w'),
-			0x2D => Some('x'),
-			0x2E => Some('c'),
-			0x2F => Some('v'),
-			0x30 => Some('b'),
-			0x31 => Some('n'),
-			0x32 => Some(','),
-			0x33 => Some(';'),
-			0x34 => Some(':'),
-			0x35 => Some('='),
-			// 0x36 => Some(''), Shift droite
-			// 0x37 => Some(''),
-			// 0x38 => Some(''), alt gauche
-			0x39 => Some(' '),
-			_ => None,
-	    };
 
-		if let Some(key) = key
+		if let Some(key) = char_from_scancode(scancode)
 		{
-			print!("{}", key);
+            unsafe
+            {
+			    print!("{}{}{}", if keyboard_states.is_ctrl { "^" } else { "" }, key, if keyboard_states.is_ctrl { "\n" } else { "" });
+            }
 		}
 		else
 		{
-			if scancode & 0x80 == 0
+			unsafe
 			{
-				println!("scancode: {:#x}", scancode);
+				match scancode
+				{
+					0x2A => keyboard_states.is_shift = true,
+					0xAA => keyboard_states.is_shift = false,
+					0x1D => keyboard_states.is_ctrl = true,
+					0x9D => keyboard_states.is_ctrl = false,
+					_ => if scancode % 0x80 == 0
+					{
+						println!("scancode: {:#x}", scancode);
+					}
+				};
 			}
 		}
 	}
