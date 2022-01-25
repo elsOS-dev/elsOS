78248836eef24f48be970e8476749c67b15ae461
afrangio <afrangio@student.42.fr>
2022-01-25 12:37:50 +0100

    vga: remove dependencies

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -128,46 +128,29 @@ impl fmt::Write for Writer
 	}
 }
 
-//use lazy_static::lazy_static;
-use spin::Mutex;
-
-//
-//lazy_static!
-//{
-//	pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer
-//	{
-//        column_position: 0,
-//        color_code: ColorCode::new(Color::Yellow, Color::Black),
-//        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-//    });
-//}
-
 #[macro_export]
-macro_rules! print {
+macro_rules! print
+{
     ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
 }
 
 #[macro_export]
-macro_rules! println {
+macro_rules! println
+{
     () => ($crate::print!("\n"));
     ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
 }
 
 #[doc(hidden)]
-pub fn _print(args: fmt::Arguments) {
+pub fn _print(args: fmt::Arguments)
+{
     use core::fmt::Write;
-	let w: Mutex<Writer> = Mutex::new(Writer{
+	let mut w = Writer {
 		column_position: 0,
 		color_code: ColorCode::new(Color::Yellow, Color::Black),
 		buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-	});
+	};
 
-	if let Some(s) = args.as_str() {
-		w.lock().write_str(s);
-    } else {
-		w.lock().write_str(&args.as_str().unwrap());
-    }
-
-	//w.lock().write_str(args.as_str().unwrap()).unwrap();
+	w.write_fmt(args).unwrap();
 }
 
