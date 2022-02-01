a96bc33e31bd85de3d3d79fd76596031292dd489
Edgar Fouillet <git@edgar.fouillet.eu>
2022-02-01 21:23:30 +0100

    tools: add scripts to build and install grub cli on macOS

---
+++ tools/build_grub.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone git://git.savannah.gnu.org/grub.git
+pushd grub
+	./bootstrap
+	./autogen.sh
+	mkdir build
+	pushd build
+		../configure --disable-werror TARGET_CC=i686-elf-gcc TARGET_OBJCOPY=i686-elf-objcopy TARGET_STRIP=i686-elf-strip TARGET_NM=i686-elf-nm TARGET_RANLIB=i686-elf-ranlib --target=i686-elf
+		make -j$(nproc)
+		sudo make install
+	popd
+popd
