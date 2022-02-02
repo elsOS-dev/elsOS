848dee97501cd77c624a5fe1fb6521e28c93e65f
Edgar Fouillet <git@edgar.fouillet.eu>
2022-02-02 06:59:57 +0100

    serial: initial support

--- Makefile
+++ Makefile
@@ -30,7 +30,7 @@ assembly_object_files := $(patsubst src/arch/$(arch)/%.asm, \
 all: $(kernel)
 
 run: $(iso)
-	@qemu-system-x86_64 -drive format=raw,file=$(iso)
+	@qemu-system-x86_64 -drive format=raw,file=$(iso) -serial stdio
 
 iso: $(iso)
 
--- src/elsos.rs
+++ src/elsos.rs
@@ -11,6 +11,7 @@ mod vga;
 mod keyboard;
 mod tty;
 mod multiboot;
+mod serial;
 
 use core::panic::PanicInfo;
 
@@ -22,26 +23,54 @@ static EXTRAVERSION: &str = env!("EXTRAVERSION");
 #[no_mangle]
 pub extern "C" fn kernel_main(magic: u32, address: u32)
 {
+	init_serial();
 	vga::cursor::init(0, 15);
 	if multiboot::check_magic(magic) && multiboot::parse(address)
 	{
-		println!("\n");
-		println!("        :::      ::::::::     __ _  ____  ____  ");
-		println!("      :+:      :+:    :+:    (  / )(  __)/ ___) ");
-		println!("    +:+ +:+         +:+      |   ( |  _) \\___ \\ ");
-		println!("  +#+  +:+       +#+         (__\\_)(__)  (____/ ");
-		println!("+#+#+#+#+#+   +#+           ");
-		println!("     #+#    #+#              Willkumme uf elsOS {}.{}.{}{}", VERSION, PATCHLEVEL, SUBLEVEL, EXTRAVERSION);
-		println!("    ###   #########          Hello, kernel world !");
+		logln!("\n");
+		logln!("        :::      ::::::::     __ _  ____  ____  ");
+		logln!("      :+:      :+:    :+:    |  / )|  __)/ ___) ");
+		logln!("    +:+ +:+         +:+      |   ( |  _) \\___ \\ ");
+		logln!("  +#+  +:+       +#+         |__\\_)|__)  (____/ ");
+		logln!("+#+#+#+#+#+   +#+           ");
+		logln!("     #+#    #+#              Willkumme uf elsOS {}.{}.{}{}", VERSION, PATCHLEVEL, SUBLEVEL, EXTRAVERSION);
+		logln!("    ###   #########          Hello, kernel world !");
 
 		print!("\x1B41;32mHenlo\x1B38;48m");
 		keyboard::get_scancodes();
 	}
 }
 
+fn init_serial()
+{
+	println!("[{}] init serial", boot::ok_fail(serial::init(serial::COM1)));
+}
+
 #[panic_handler]
 fn panic(info: &PanicInfo) -> !
 {
-	println!("{}", info);
+	logln!("{}", info);
 	loop {}
+}
+
+#[macro_export]
+macro_rules! log
+{
+	($($arg:tt)*) => ($crate::tty::_print(format_args!($($arg)*)));
+	($($arg:tt)*) => ($crate::serial::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! logln
+{
+	() =>
+	{
+		($crate::print!("\n"));
+		($crate::serial_print!("\n"));
+	};
+	($($arg:tt)*) =>
+	{
+		($crate::print!("{}\n", format_args!($($arg)*)));
+		($crate::serial_print!("{}\n", format_args!($($arg)*)));
+	}
 }
