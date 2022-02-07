824d479ac4f6c7f11658a4c14db49757921951f5
Edgar Fouillet <git@edgar.fouillet.eu>
2022-02-07 11:22:07 +0100

    commands: add reboot and clear, handle empty command

--- src/tty/basic_commands.rs
+++ src/tty/basic_commands.rs
@@ -1,13 +1,24 @@
+use core::arch::asm;
 use crate::utilities;
+use crate::vga;
 
 pub fn execute(command: &str)
 {
 	match command
 	{
-		"halt" | "exit" => halt(),
 		"help" => help(),
+		"clear" => clear(),
+		"halt" | "exit" => halt(),
+		"reboot" => reboot(),
+		"" => {},
 		_ => crate::println!("{}: unknown command. Use help for more", command)
 	};
+}
+
+fn clear()
+{
+	super::Tty::current().clear();
+	vga::Buffer::clear();
 }
 
 fn halt()
@@ -15,9 +26,22 @@ fn halt()
 	utilities::shutdown_qemu();
 }
 
+fn reboot()
+{
+	// jump to the post procedure to reboot
+	unsafe
+	{
+		asm!("push 0FFFFh");
+		asm!("push 0000h");
+		asm!("retf");
+	}
+}
+
 fn help()
 {
 	crate::println!("Supported commands:");
-	crate::println!("halt | exit: stop the virtual machine (qemu only)");
-	crate::println!("help: show this help");
+	crate::println!("  help:        show this help");
+	crate::println!("  clear:       clear the screen");
+	crate::println!("  halt | exit: stop the virtual machine (qemu only)");
+	crate::println!("  reboot:      reboot the machine");
 }
