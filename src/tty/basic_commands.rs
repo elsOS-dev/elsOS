b360ad1eaa0128d059e3009100db0ce61d2ca001
Edgar Fouillet <git@edgar.fouillet.eu>
2022-02-11 19:49:14 +0100

    commands: add printmem / pm and panic debug commands

--- src/tty/basic_commands.rs
+++ src/tty/basic_commands.rs
@@ -11,6 +11,8 @@ pub fn execute(command: &str)
 		"halt" | "exit" => halt(),
 		"reboot" => reboot(),
 		"scheen" => scheen(),
+		"pm" |"printmem" => printmem(),
+		"panic" => panic(),
 		"" => {},
 		_ => crate::println!("{}: unknown command. Use help for more", command)
 	};
@@ -43,6 +45,22 @@ fn scheen()
 	crate::println!("yo yo des esch d'becht OS eh ?");
 }
 
+fn printmem()
+{
+	crate::println!("ok");
+	crate::serial_println!("==============");
+	for i in 0..super::BUFFER_SIZE
+	{
+		crate::serial_print!("{}", super::Tty::current().chars[i] as char);
+	}
+	crate::serial_println!("==============");
+}
+
+fn panic()
+{
+	panic!("panic()");
+}
+
 fn help()
 {
 	crate::println!("Supported commands:");
@@ -50,4 +68,7 @@ fn help()
 	crate::println!("  clear:       clear the screen");
 	crate::println!("  halt | exit: stop the virtual machine (qemu only)");
 	crate::println!("  reboot:      reboot the machine");
+	crate::println!("Debug commands:");
+	crate::println!("  printmem | pm: print current tty buffer to serial");
+	crate::println!("  panic        : trigger a rust panic");
 }
