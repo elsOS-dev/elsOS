436ced630573556e8c581625ceab1c356bb423d1
afrangio <afrangio@student.42.fr>
2022-03-08 13:26:46 +0100

    mem: marking reserved pages as reserved in bitmap
    The page frame allocator does now mark as "reserved" (set bit to 1 on bitmap)
    The reserved pages include grub marked as reserved and the kernel in memory and
    finally the bitmap itself.

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -9,38 +9,38 @@ extern "C"
 	static _kernel_end: c_void;
 }
 
-pub fn get_mem_size(mmap: *const MultibootTagMmap, mmap_size: usize) -> u64
+pub fn get_mem_size(mmap: *const MultibootTagMmap, mmap_size: usize) -> usize
 {
-	static mut MEM_SIZE_BYTES: u64 = 0;
+	let mut mem_size_bytes: u64 = 0;
 	unsafe
 	{
-		if MEM_SIZE_BYTES > 0
-		{
-			return MEM_SIZE_BYTES;
-		}
-		// crate::logln!("\x1B[33mmmap: {:#x?}\x1B[39m", (*mmap).entries(mmap_size));
+		crate::logln!("\x1B[33mmmap: {:#x?}\x1B[39m", (*mmap).entries(mmap_size));
 		for mmap_entry in (*mmap).entries(mmap_size)
 		{
-			MEM_SIZE_BYTES += mmap_entry.len as u64;
+			mem_size_bytes += mmap_entry.len as u64;
 		}
-		if MEM_SIZE_BYTES > usize::MAX as u64
+		if mem_size_bytes > usize::MAX as u64
 		{
-			panic!("This version of ElsOS is in 32 bit, it only supports {}Mo of RAM, you have {}Mo installed", (usize::MAX / 1024) / 1024, (MEM_SIZE_BYTES / 1024) / 1024);
+			panic!("This version of ElsOS is in 32 bit, it only supports {}Mo of RAM, you have {}Mo installed", (usize::MAX / 1024) / 1024, (mem_size_bytes / 1024) / 1024);
 		}
-		return MEM_SIZE_BYTES;
+		return mem_size_bytes as usize;
 	}
 }
 
-pub fn get_kernel_size() -> usize
+pub fn get_largest_mem_seg(mmap: *const MultibootTagMmap, mmap_size: usize) -> usize
 {
-	let kernel_start: usize;
-	let kernel_end: usize;
+	let mut largest_free_mem_seg: usize = 0;
+	let mut largest_free_mem_seg_size: usize = 0;
 	unsafe
 	{
-		kernel_start = &_kernel_start as *const _ as usize;
-		kernel_end =  &_kernel_end as *const _ as usize;
+		for entry in (*mmap).entries(mmap_size)
+		{
+			if entry.len as usize > largest_free_mem_seg_size
+			{
+				largest_free_mem_seg_size = entry.len as usize;
+				largest_free_mem_seg = entry.addr as usize;
+			}
+		}
 	}
-	let kernel_size = kernel_end - kernel_start;
-	crate::logln!("{:#x?} {:#x?}", kernel_start, kernel_end);
-	return kernel_size;
+	largest_free_mem_seg
 }
