842064be2af4ec74eb9b71f9ddac639db303c0db
Edgar Fouillet <git@edgar.fouillet.eu>
2022-03-21 22:45:24 +0100

    build: improve makefile, move linker script

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main()
 {
-	println!("cargo:rustc-link-search=native=./");
+	println!("cargo:rustc-link-search=native=./build/libc");
 	println!("cargo:rustc-link-lib=static=c");
-	println!("cargo:rerun-if-changed=libc.a");
+	println!("cargo:rerun-if-changed=build/libc");
 }
