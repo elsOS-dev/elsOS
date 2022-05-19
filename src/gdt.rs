e9eac75edb7d12b301d18c5d85df1d3e843d97e0
afrangio <afrangio@student.42.fr>
2022-05-19 13:24:10 +0200

    gdt: documenting our values

--- src/gdt.rs
+++ src/gdt.rs
@@ -34,6 +34,61 @@ struct gdt
 	user_stack: gdt_entry,
 }
 
+
+// Base bit //
+// We choose to begin each segment at 0, so each segments covers the entire
+// memory.
+
+// Access bit //
+// First bit, is present bit, should be always 1 except for the null entry
+
+// Second and third bit, is the privilege level, 00 means highest privilege, 
+// so every kernel segment has 00, 11 is the lowest privilege level, so every
+// user segment has 11.
+
+// fourth bit is defines if the segment is a system segment or a code/data 
+// segment, we don't have any system segments yet so it should be 1 on each 
+// segment.
+
+// The fifth bit, is the executable bit. If it's a code segment that should be
+// executable it has to bet set to 1 like in kernel and user code segments.
+
+// The sixth bit, depends on the type of segment:
+// for data segments, it defines if the data grows up or down like in kernel 
+// and user stack. Because the stack grows down.
+// for code segments, 0 means the code can only be executed from the same ring.
+// and 1 means it can be executed by equal or lower ring levels.
+// we choose 0 because it is likely the more secure option.
+
+// The seventh is the R/W bit:
+// for code segments it's a readable bit. if 0 read is not allowed, 
+// write is never allowed.
+// for data segments: writable bit: if 0, write is not allowed, read is always
+// allowed.
+// We choose or kernel segments and user segments to be readable and writable,
+// because we want them to be more permissive, and in the future tweak them if
+// needed in a more restrictive way.
+
+// The last bit must be set to 0 it is set to 1 when the CPU accesses the 
+// segment
+
+
+// Flags and Limits //
+// Flags are composed with 4 bits,
+// c means 1100
+// first bit: Granularity flag: if 0 the blocks are 1 byte, if 1 4 kbytes
+// second bit: processor mode, 0 16 bit, 1: 32 bit protected
+// third bit: if set to 1, defines 64 bit
+// fourth bit: reserved
+//  we are in 32 bit protected using the 4kbytes blocks, this makes 1100, or C
+// in hexadecimal.
+// 5-8: last bit of the 5 bit limits value, makes for a total of 20 bits:
+// A 20-bit value, tells the maximum addressable unit, either in 1 byte
+// units, or in 4KiB pages. Hence, if you choose page granularity and set the
+// Limit value to 0xFFFFF the segment will span the full 4 GiB address space
+//  in 32-bit mode. 
+
+
 static mut GDT_TABLE: gdt = gdt
 {
 	null: gdt_entry
