dd6aa0d59d33b50b0e304c93533280209e0555cc
Edgar Fouillet <git@edgar.fouillet.eu>
2022-10-10 00:44:11 +0200

    libc: add strlen for the rust toolchain

--- src/libc/libc.h
+++ src/libc/libc.h
@@ -1,9 +1,12 @@
 #ifndef LIBC_H
 # define LIBC_H
 
+# include <stddef.h>
+
 void	*memset(void *b, int c, size_t len);
 void	*memcpy(void *dst, const void *src, size_t n);
 void	*memmove(void *dst, const void *src, size_t len);
 int		memcmp(const void *s1, const void *s2, size_t n);
+size_t	strlen(const char *s1);
 
 #endif
